2f3e8d174dd19cd439721b3ab7cc72963ddc0bba
Ronaldo <ronaldo.moraes1990@gmail.com>
2017-12-21 21:26:02 -0200

    #13 Swagger local com json do swaghub

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,20 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <a style="text-decoration: none;" href="{{ asset('swagger/index.html') }}">
+                        Swagger UI
+                    </a>
                 </div>
-
+                <!--
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="{{ asset('swagger/index.html') }}">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                -->
             </div>
         </div>
     </body>
