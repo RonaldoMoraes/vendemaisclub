958a1b2b2f4fa94ec5dfad9b21ec70040b914e34
Ronaldo <ronaldo.moraes1990@gmail.com>
2017-12-22 18:14:08 -0200

    #13 Mission Complete.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Class UserController
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="",
+ *     host="localhost:8000",
+ *     schemes={"http"},
+ *     @SWG\Info(
+ *          description= "API do teste proposto pela Vende+ Club",
+ *          version= "1.0.0",
+ *          title= "Teste Vende+ API",
+ *         @SWG\Contact(name="Ronaldo Moraes", email="ronaldo.moraes1990@gmail.com"),
+ *     )
+ *     
+ * )
+ */
+
+class ApiController extends Controller{
+
+}
